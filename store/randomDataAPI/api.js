31c440d1007587c6bead4a7f047ec0b1c944d344
Crowdbotics <engineering@crowdbotics.com>
2023-09-14 20:21:03 +0000

    Update store.

--- store/randomDataAPI/api.js
+++ store/randomDataAPI/api.js
@@ -3,7 +3,7 @@ const randomDataAPI = axios.create({
   baseURL: "https://random-data-api.com/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
-function randomdataapi2_get_api_users_random_user_read(payload) {
+function randomdataapi_get_api_users_random_user_read(payload) {
   return randomDataAPI.get(`/api/users/random_user`)
 }
-export const apiService = { randomdataapi2_get_api_users_random_user_read }
+export const apiService = { randomdataapi_get_api_users_random_user_read }
